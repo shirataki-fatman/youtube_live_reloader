35eac02b129e9c64bb80026de08675ecb54b360b
ooshiro <tatsunori272@gmail.com>
2020-10-31 05:07:52 +0900

    不要なものを削除

--- watch.js
+++ watch.js
@@ -5,14 +5,14 @@ const patienceTime = 1000;
 const main = () => {
     const video = document.querySelector('.html5-main-video')
     video.addEventListener("waiting", () => {
-        console.log("wait start")
         watchWaiting = true
         waitStartTime = new Date().getTime()
     })
     video.addEventListener("canplay", () => {
-        console.log("wait canceled")
-        watchWaiting = false
-        waitStartTime = null
+        if (watchWaiting) {
+            watchWaiting = false
+            waitStartTime = null
+        }
     })
     setInterval(() => {
         if (watchWaiting && new Date().getTime() - waitStartTime > patienceTime) {
