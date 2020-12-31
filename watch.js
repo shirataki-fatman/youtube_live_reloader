40b9248a4365627f76aacaf490b247790c5f2ff6
ooshiro <tatsunori272@gmail.com>
2020-12-31 18:29:08 +0900

    生放送以外でもリロードしてしまう問題を修正

--- watch.js
+++ watch.js
@@ -3,22 +3,30 @@ let waitStartTime = null;
 const patienceTime = 1000;
 
 const main = () => {
-    const video = document.querySelector('.html5-main-video')
-    video.addEventListener("waiting", () => {
-        watchWaiting = true
-        waitStartTime = new Date().getTime()
-    })
-    video.addEventListener("canplay", () => {
-        if (watchWaiting) {
-            watchWaiting = false
-            waitStartTime = null
-        }
-    })
-    setInterval(() => {
-        if (watchWaiting && new Date().getTime() - waitStartTime > patienceTime) {
-            window.location.reload()            
-        }
-    }, 1000)
+    var isLive = false;
+    var liveButton = document.querySelector('.ytp-live-badge');
+    if (liveButton && !liveButton.getAttribute("disabled")) {
+        isLive = true;
+    }
+
+    if (isLive) {
+        const video = document.querySelector('.html5-main-video')
+        video.addEventListener("waiting", () => {
+            watchWaiting = true
+            waitStartTime = new Date().getTime()
+        })
+        video.addEventListener("canplay", () => {
+            if (watchWaiting) {
+                watchWaiting = false
+                waitStartTime = null
+            }
+        })
+        setInterval(() => {
+            if (watchWaiting && new Date().getTime() - waitStartTime > patienceTime) {
+                window.location.reload()            
+            }
+        }, 1000)
+    }
 }
 
 window.addEventListener("load", main, false)
